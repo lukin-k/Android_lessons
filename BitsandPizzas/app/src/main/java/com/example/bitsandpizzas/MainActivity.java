eb353b6d005f4fb145a2c5b52fa76839c951d8a0
Lukin Kirill <kirix.kl@gmail.com>
2020-07-30 10:31:47 +0300

    fuck! Android Studio wants lib androidx and don't wants appcompat. All code go fuck.

--- BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.bitsandpizzas;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
